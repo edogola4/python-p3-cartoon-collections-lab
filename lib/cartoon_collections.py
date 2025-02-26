e002a0a510b67dda813917724844a82ff66cb021
edogola4 <edogola4@gmail.com>
2025-02-26 21:21:25 +0300

    Test Passing

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,21 @@
-def roll_call_dwarves():
+def roll_call_dwarves(dwarves):
+    for i, dwarf in enumerate(dwarves):
+        print(f"{i + 1}. {dwarf}")
     pass
 
-def summon_captain_planet():
+def summon_captain_planet(calls):
+    return [call.capitalize() + "!" for call in calls]
+
     pass
 
-def long_planeteer_calls():
+def long_planeteer_calls(calls):
+    return any(len(call) > 4 for call in calls)
     pass
 
-def find_the_cheese():
+def find_the_cheese(foods):
+    cheeses = {"cheddar", "gouda", "camembert"}
+    for food in foods:
+        if food in cheeses:
+            return food
+    return None
     pass
